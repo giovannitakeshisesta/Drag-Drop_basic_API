9eccca7e14c00336cf0f6fa0ac7d9a63a5a49a7b
“Giovanni” <giovannitakeshisesta@gmail.com>
2022-06-12 21:30:20 +0200

    .no deploy...

--- config/db.config.js
+++ config/db.config.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose')
 
 const DB_NAME = 'listDND';
-const URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017';
-const DB_URI = `${URI}/${DB_NAME}`;
-
-// const DB_URI = 'mongodb://127.0.0.1:27017/listDND';
-
+const DB_URI = 'mongodb://127.0.0.1:27017/listDND';
 
 mongoose
   .connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
